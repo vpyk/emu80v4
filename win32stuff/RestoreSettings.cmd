6adacf0b929a15a0cc4363942aad1b7822fdedb3
Viktor Pykhonin <pyk@mail.ru>
2018-09-24 21:30:16 +0300

    Windows cmd-script for restoring emulator settings

---
+++ win32stuff/RestoreSettings.cmd
@@ -0,0 +1,6 @@
+set LOCAL_SETTINGS=%~dp0_settings\emu80.ini
+set PROFILE_SETTINGS=%USERPROFILE%\AppData\Roaming\Emu80\emu80.ini
+
+del %LOCAL_SETTINGS% /f
+del %PROFILE_SETTINGS% /f
+                     
