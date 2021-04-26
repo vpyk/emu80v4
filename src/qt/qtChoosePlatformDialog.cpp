eefe9b5a9ab98d7c0544014aeb8f5f9a2487b3a6
Viktor Pykhonin <pyk@mail.ru>
2021-04-26 23:53:01 +0300

    Highlight basic platforms in the platform selection dialog

--- src/qt/qtChoosePlatformDialog.cpp
+++ src/qt/qtChoosePlatformDialog.cpp
@@ -44,11 +44,11 @@ bool ChoosePlatformDialog::execute(std::vector<PlatformInfo>& pi, int& pos, bool
         QString s = QString::fromUtf8((*it).platformName.c_str());
         QListWidgetItem* item = new QListWidgetItem(s);
         ui->platformListWidget->addItem(item);
-        /*if ((*it).objName.find(".") == std::string::npos) {
+        if ((*it).objName.find(".") == std::string::npos || (*it).objName == "orion.2") { //todo rename orion.2 and remove check
             QFont font = item->font();
             font.setBold(true);
             item->setFont(font);
-        }*/
+        }
     }
     ui->platformListWidget->setCurrentRow(pos);
     if (exec() == QDialog::Accepted) {
