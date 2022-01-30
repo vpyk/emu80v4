9c16ff99fafafd67d51ce66ab46ea872dbaaf017
Viktor Pykhonin <pyk@mail.ru>
2022-01-30 19:11:07 +0300

    Korvet: Fix crash under Windows XP

--- src/Korvet.cpp
+++ src/Korvet.cpp
@@ -566,16 +566,16 @@ void KorvetLutRegister::writeByte(int, uint8_t value)
 
 KorvetGraphicsAdapter::KorvetGraphicsAdapter()
 {
-    for (int i = 0; i < 4; i++) {
-        m_planes[i] = new uint8_t[0x10000];
-        memset(m_planes[i], 0, 0x10000);
+    for (int i = 0; i < 3; i++) {
+        m_planes[i] = new uint8_t[0x4000];
+        memset(m_planes[i], 0, 0x4000);
     }
 }
 
 
 KorvetGraphicsAdapter::~KorvetGraphicsAdapter()
 {
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 3; i++)
         delete[] m_planes[i];
 }
 
