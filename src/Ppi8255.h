f5d7bfbcd19e6bf81c439dbf3d9a4e6d2a2d8bab
Viktor Pykhonin <pyk@mail.ru>
2022-12-13 14:01:00 +0300

    Add new property noReset for PPI 8255
    Set to "yes" when reset input is not used

--- src/Ppi8255.h
+++ src/Ppi8255.h
@@ -1,6 +1,6 @@
 ﻿/*
  *  Emu80 v. 4.x
- *  © Viktor Pykhonin <pyk@mail.ru>, 2016-2018
+ *  © Viktor Pykhonin <pyk@mail.ru>, 2016-2022
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -79,6 +79,8 @@ class Ppi8255 : public AddressableDevice
         PpiChMode m_chBMode;
         PpiChMode m_chCHiMode;
         PpiChMode m_chCLoMode;
+
+        bool m_noReset = false; // true if RESET input is not used
 };
 
 #endif // PPI8255_H
