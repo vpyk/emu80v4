248259e977a5c6f634cc0d7a62d74d83ae0cdb23
Viktor Pykhonin <pyk@mail.ru>
2023-01-20 20:55:54 +0300

    Increase version number

--- src/Version.h
+++ src/Version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_STR "4.0.439"
-#define VER_COMMA_SEP 4,0,439,0
+#define VER_STR "4.0.440"
+#define VER_COMMA_SEP 4,0,440,0
 
-#define VI_FILE_DESCR "Emu80 main executable"
+#define VI_FILE_DESCR "Emu80 executable"
 #define VI_COPYRIGHT "Copyright © Viktor Pykhonin, 2016-2022"
 #define VI_FILE_NAME "Emu80qt.exe"
 #define VI_PRODUCT "Emu80"
