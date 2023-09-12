26ced6a26c56e4c2789d63ba9fe3e4ac90b284fc
Viktor Pykhonin <pyk@mail.ru>
2023-09-12 22:30:16 +0300

    Update documentation

--- src/Version.h
+++ src/Version.h
@@ -19,8 +19,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_STR "4.0.455"
-#define VER_COMMA_SEP 4,0,455,0
+#define VER_STR "4.0.456"
+#define VER_COMMA_SEP 4,0,456,0
 
 #define VI_FILE_DESCR "Emu80 executable"
 #define VI_COPYRIGHT "Copyright © Viktor Pykhonin, 2016-2023"
